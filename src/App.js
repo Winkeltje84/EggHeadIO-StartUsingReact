020ffe8991200c194ebf527d53a518dea62600d2
Winkeltje84 <bwinkel@gmail.com>
2017-07-27 23:33:34 +0200

    creating a Prop called "txt" and using it in the component as this.props.txt

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 class App extends React.Component {
   render(){
+    let txt = this.props.txt;
     return (
       <div>
         <h1>This is a component</h1>
         <p>when adding an extra tag it needs to be wrapped into
         one, because only one can be returned.</p>
+        <p>{txt}</p>
       </div>
     )
   }
