4ab4fc0ca6f2e24f54a793efed08c389956651ea
Winkeltje84 <bwinkel@gmail.com>
2017-07-27 23:26:02 +0200

    when adding multiple tags, you need to wrap it into one div, otherwise it breaks

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 class App extends React.Component {
   render(){
-    return <h1>This is a component</h1>
+    return (
+      <div>
+        <h1>This is a component</h1>
+        <p>when adding an extra tag it needs to be wrapped into
+        one, because only one can be returned.</p>
+      </div>
+    )
   }
 }
 
