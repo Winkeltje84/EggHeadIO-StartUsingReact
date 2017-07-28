381af3fb497d38da84dd7eb6027f2da49b0f5a0a
Winkeltje84 <bwinkel@gmail.com>
2017-07-28 16:49:04 +0200

    adding PropTypes with 'prop-types' package --> adding functionality that checks whether props have correct type and/or whether it is present if it should (isRequired)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class App extends React.Component {
   render(){
     let txt = this.props.txt;
+    let nr = this.props.nr;
     return (
       <div>
         <h1>This is a component</h1>
         <p>when adding an extra tag it needs to be wrapped into
         one, because only one can be returned.</p>
-        <p>{txt}</p>
+        <p>some text from props: {txt}</p>
+        <p>a number from props: {nr}</p>
       </div>
     )
   }
+}
+
+App.propTypes = {
+  txt: PropTypes.string,
+  nr: PropTypes.number.isRequired
 }
 
 export default App
