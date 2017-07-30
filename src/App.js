78a62222458da45a2fbfdd15c51b5a6062d6c492
Winkeltje84 <bwinkel@gmail.com>
2017-07-30 15:20:46 +0200

    setting state with constructor -> super() & showing the state in the render function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class App extends React.Component {
+  constructor(){
+    super(); // super() will give the keyword 'this' the context within our component rather then its parent class React.Component
+    this.state = {
+      someText: 'This is the state "someText"'
+    }
+  }
+
   render(){
     let txt = this.props.txt;
     let nr = this.props.nr;
@@ -12,6 +19,7 @@ class App extends React.Component {
         one, because only one can be returned.</p>
         <p>some text from props: {txt}</p>
         <p>a number from props: {nr}</p>
+        <h3>{this.state.someText}</h3>
       </div>
     )
   }
