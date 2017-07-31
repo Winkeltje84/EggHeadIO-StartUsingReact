257b3d8924571de473e18913cf3f8a96889472b7
Winkeltje84 <bwinkel@gmail.com>
2017-07-31 16:26:06 +0200

    using API to get Starwars data & map over found data --> showing starship names

--- src/App.js
+++ src/App.js
@@ -1,49 +1,30 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 class App extends React.Component {
   constructor(){
-    super(); // super() will give the keyword 'this' the context within our component rather then its parent class React.Component
+    super(); 
     this.state = {
-      someText: 'This is the initial state "someText"'
+      items : []
     }
   }
 
-  update ( e ){
-    this.setState( {someText: e.target.value} )
+  componentWillMount(){
+    // API documentation --> https://swapi.co/documentation#json
+    fetch( 'https://swapi.co/api/starships/')
+      .then( response => response.json() )
+      // .then( data => {} )
+      // get the 'results' from the data -> call it 'items'
+      // => setState --> set the items props to the value of 'items' given by the data
+      .then( ({results: items}) => this.setState({ items }))
   }
-
   render(){
+    let items = this.state.items
     return (
       <div>
-        <h3>{this.state.someText}</h3>
-        <Widget update={this.update.bind(this)}/>
-        <Widget update={this.update.bind(this)}/>
+        {items.map(item => <h4>{item.name}</h4>)}
       </div>
     )
   }
-}
-
-const Widget = (props) => // new child component that takes in props given by parent and updates the state of its parent
-  <input type="text" onChange={props.update}/> // it calls the update prop when onChange
-
-App.propTypes = {
-  // custom propTypes error handling:
-  // 3 arguments: props= all props component has received / propName = name for this particular prop / component
-  txt(props, propName, component){
-    if(!(propName in props)){
-      return new Error(`Missing ${propName}`)
-    }
-    if(props[propName].length > 10){
-      console.log('this should return error')
-      return new Error(`prop ${propName} was longer than 10 characters!`)
-    }
-  },
-  nr: PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  txt: "this is the default 'txt' if no prop of 'txt' is added by parent"
 }
 
 export default App
