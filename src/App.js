394df353859eec973379a74390a46b1e65032403
Winkeltje84 <bwinkel@gmail.com>
2017-07-31 17:42:10 +0200

    adding a filter function that itterates that total results and only filters out the ones that have text which is entered into the input field

--- src/App.js
+++ src/App.js
@@ -17,10 +17,29 @@ class App extends React.Component {
       // => setState --> set the items props to the value of 'items' given by the data
       .then( ({results: items}) => this.setState({ items }))
   }
+  filterFunction(e){
+    // filterFunction is called by onChange property of input field
+    this.setState({ filter: e.target.value})
+    // a prop 'filter' added to the state with the value of what is in the input field
+  }
   render(){
     let items = this.state.items
+    // if prop 'filter' in state has a value (= true)
+    if(this.state.filter){
+      // set prop 'items' equal to --> the result of filter
+      items = items.filter( item =>
+        // check for every item (which is set to lowercase)
+        item.name.toLowerCase()
+          .includes(this.state.filter.toLowerCase())
+          // whether is includes : text that is present in the 'filter' prop (turned lowercase)
+        // and only the items are filtered out that include the output of the filter function
+        // that filter function just returns all items that return true
+        // & are put into 'items'
+      )
+    }
     return (
       <div>
+        <input type="text" onChange={this.filterFunction.bind(this)}/>
         {items.map(item =>
           <Starship key={item.name} starship={item} />
         )}
